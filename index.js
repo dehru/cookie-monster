d382abd1fff55137121ba8e218e2788733c64036
Dehru Cromer <dehru.cromer@salesforce.com>
2020-02-26 14:00:34 -0700

    add body and cookie parser

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(cookieParser())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .post('/', (req, res) => {
+    console.log('Got body:', req.body);
+    res.cookie('token', req.body.token, { maxAge: 900000, httpOnly: true });
+    res.status(200).send('hello i got your token here and set in a cooke named "token": ' + req.body.token);
+    res.end();
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
