62545c8ea952cd64f8502a934a585ffc2bdfd782
Dehru Cromer <dehru.cromer@salesforce.com>
2020-03-02 10:36:09 -0700

    a little javascript

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+function getHtml(body) {
+  return `
+<html>
+<head>
+<script>
+window.addEventListener("message", receiveMessage, false);
+function receiveMessage(event) {
+  console.log('message received: ', event);
+}
+</script>
+</head>
+<body>
+${body}
+</body>
+</html>
+`};
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: true }))
   .use(cookieParser())
   .get('/', (req, res) => {
-    res.status(200).send('HEROKU - This will be an hidden i-frame');
+    res.status(200)
+      .set('Content-Type', 'text/html')
+      .send(getHtml('HEROKU frame waiting for postMessage'));
     res.end();
   })
   .post('/', (req, res) => {
-    res.cookie('token', req.body.token, { maxAge: 900000, httpOnly: true });
-    res.status(200).send(`HEROKU Cookie monster ate your posted "token": ${req.body.token}`);
+    res.cookie('token', req.body.token, { maxAge: 900000, httpOnly: true })
+      .set('Content-Type', 'text/html')
+      .status(200)
+      .send(`HEROKU Cookie monster ate your posted "token": ${req.body.token}`);
     res.end();
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
