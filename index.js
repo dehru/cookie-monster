ecab028903efe51cef42223d7b808b0635e848b5
Dehru Cromer <dehru.cromer@salesforce.com>
2020-03-02 15:59:58 -0700

    adding some more logs

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ const nocache = require('nocache');
 function getHtml(body) {
   return `
 <html>
-<head>
-<script>
-  window.addEventListener("message", receiveMessage, false);
-  function receiveMessage(event) {
-    console.log('message received: ', event);
-    if (event && event.data) {
-      const token = event.data;
-      fetch('./', { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: "token=" + token})
-        .then((response) => { 
-          if (response.ok) { 
-            window.location.href="./?" + new Date().getTime(); 
-          } 
-        } 
-      );
-    }
-  }
-</script>
-</head>
-<body>
-${body}
-</body>
+  <head>
+    <script>
+      window.addEventListener("message", receiveMessage, false);
+      function receiveMessage(event) {
+        console.log('message received: ', event);
+        if (event && event.data) {
+          const token = event.data;
+          fetch('./', { method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: "token=" + token})
+            .then((response) => { 
+              if (response.ok) { 
+                window.location.href="./?" + new Date().getTime(); 
+              } 
+            } 
+          );
+        }
+      }
+    </script>
+  </head>
+  <body>
+    ${body}
+  </body>
 </html>
 `};
 
@@ -39,7 +39,7 @@ express()
   .use(nocache())
   .set('etag', false)
   .get('/', (req, res) => {
-    console.log('cookie monster sees: ', req.cookies);
+    console.log('GET / cookie monster sees: ', req.cookies);
     const token = req.cookies.token;
     res.status(200)
       .set('Content-Type', 'text/html')
@@ -47,6 +47,7 @@ express()
     res.end();
   })
   .post('/', (req, res) => {
+    console.log('POST / body is: ', req.body);
     res.cookie('token', req.body.token, { maxAge: 900000, httpOnly: true, sameSite: 'None', secure: true })
       .set('Content-Type', 'text/html')
       .redirect('./');
