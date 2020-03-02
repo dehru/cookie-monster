b4a1808ce3f529a92cdfed2eff42d2e303c6becf
Dehru Cromer <dehru.cromer@salesforce.com>
2020-03-02 16:17:38 -0700

    try lax cookie

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ express()
   })
   .post('/', (req, res) => {
     console.log('POST / body is: ', req.body);
-    res.cookie('token', req.body.token, { maxAge: 900000, httpOnly: true, sameSite: 'None', secure: true })
+    res.cookie('token', req.body.token, { maxAge: 900000, httpOnly: true, sameSite: 'Lax', secure: true })
       .set('Content-Type', 'text/html')
       .redirect('./');
     res.end();
