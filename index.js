8fd02562158130ab92e293fdf70d80d29027b14a
Dehru Cromer <dehru.cromer@salesforce.com>
2020-03-20 10:34:18 -0600

    postMessage

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function getHtml(body) {
     <script>
       window.addEventListener("message", receiveMessage, false);
       function acknowledgeMessage() {
-        console.log('in ack', window.parent);
+        console.log('in ack', window.top);
         debugger;
-        console.log(window.parent.postMessage);
-        window.postMessage('success', '*');
+        console.log(window.top.postMessage);
+        window.top.postMessage('success', '*');
       }
       function receiveMessage(event) {
         console.log('message received: ', event.data);
